0dfaa21b7ca172f6f7304adad4ceb0ec011df77f
thejoshcooter <thejoshcooter@gmail.com>
2021-05-15 05:40:01 -0500

    add styled global injection for css reset

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Reset from './styles/Reset'
 
 import App from './App'
 
 ReactDOM.render(
   <Router>
+    <Reset />
     <App />
   </Router>,
   document.getElementById('root')
