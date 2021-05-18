cd0178cbe2060ce1594e434a6a07f51ae1f5032c
thejoshcooter <thejoshcooter@gmail.com>
2021-05-18 01:31:53 -0500

    finish basic game loop logic and verify. setup mechanic to render notifications about current state to user.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,3 +14,27 @@ export const SLEEP_ACTION_RATE = 10
 
 export const LOVE_DECAY_RATE = 2
 export const LOVE_ACTION_RATE = 10
+
+// STATUS NOTIFICATIONS
+export const statusNotifications = {
+    egg: 'your pet is an egg, it will hatch soon',
+    happy: 'your pet is happy and healthy',
+    sad: 'your pet is feeling a bit down',
+    mad: 'your pet is very unhappy',
+    dead: 'your pet is dead',
+    hunger: {
+        full: 'your pet is well fed',
+        low: 'your pet is a little hungry',
+        critical: 'your pet is on the verge of starvation'
+    },
+    sleep: {
+        full: 'your pet is well rested',
+        low: 'your pet is getting sleepy',
+        critical: 'your pet is sleep deprived'
+    },
+    love: {
+        full: 'your pet feels loved',
+        low: 'your pet is feeling a little unloved',
+        critical: 'your pet is feeling neglected'
+    }
+}
--- src/views/game/GameNotifications.js
+++ src/views/game/GameNotifications.js
@@ -1,11 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+import { statusNotifications } from '../../utils/constants'
 
 const GameNotifications = () => {
+    const status = useSelector(state => state.game.status)
+    const hunger = useSelector(state => state.game.hunger)
+    const sleep = useSelector(state => state.game.sleep)
+    const love = useSelector(state => state.game.love)
+
+    // helpers
+    const getNotification = (status) => {
+        if (status === 'EGG') {
+            return <p>{statusNotifications['egg']}</p>
+        } else if (status === 'HAPPY') {
+            return <p>{statusNotifications['happy']}</p>
+        } else if (hunger === 0 || sleep === 0 || love === 0) {
+            return <p>{statusNotifications['dead']}</p>
+        } else if (hunger < 25 && hunger > 0) {
+            return <p>{statusNotifications['hunger'].critical}</p>
+        } else if (sleep < 25 && sleep > 0) {
+            return <p>{statusNotifications['sleep'].critical}</p>
+        } else if (love < 25 && love > 0) {
+            return <p>{statusNotifications['love'].critical}</p>
+        } else if (hunger < 50 && hunger > 25) {
+            return <p>{statusNotifications['hunger'].low}</p>
+        } else if (sleep < 50 && sleep > 25) {
+            return <p>{statusNotifications['sleep'].low}</p>
+        } else if (love < 50 && love > 25) {
+            return <p>{statusNotifications['love'].low}</p>
+        } else {
+            return <p>no notification available</p>
+        }
+    }
+    
     return (
         <>
         <Container>
-            game notifications here
+            {getNotification(status)}
         </Container>
         </>
     )
@@ -14,7 +46,6 @@ const GameNotifications = () => {
 const Container = styled.div`
     width: 100%;
     height: 20%;
-    background-color: purple;
     display: flex;
     flex-flow: column nowrap;
     justify-content: center;
